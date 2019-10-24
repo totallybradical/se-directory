3f79e7faa3b963179fe94ae95365a6d649ca9677
Bradford <bingerso@cisco.com>
2019-10-24 11:29:13 -0400

    nevermind

--- factoids/utils.py
+++ factoids/utils.py
@@ -7,12 +7,8 @@ from .views import factoid_detail
 
 def random_factoid(request):
     max_id = Factoid.objects.all().aggregate(max_id=Max("id"))['max_id']
-    if not max_id:
-        print('here')
-        return factoid_detail(request)
-    else:
-        while True:
-            pk = random.randint(1, max_id)
-            factoid = Factoid.objects.filter(pk=pk).first()
-            if factoid:
-                return factoid_detail(request, pk)
+    while True:
+        pk = random.randint(1, max_id)
+        factoid = Factoid.objects.filter(pk=pk).first()
+        if factoid:
+            return factoid_detail(request, pk)
