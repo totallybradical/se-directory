d28c24389909302c7d072f7d28a297e759cdc176
Bradford <bingerso@cisco.com>
2019-10-30 15:02:43 -0400

    Fixed major bug

--- profiles/urls.py
+++ profiles/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.profile_list, name='profile_list'),
-    path('<geo>/', views.profile_list, name='profile_list'),
-    path('<cec>/', views.profile_detail, name='profile_detail'),
-    path('<cec>/edit/', views.edit_profile, name='edit_profile'),
+    path('geos/<geo>/', views.profile_list, name='profile_list'),
+    path('se/<cec>/', views.profile_detail, name='profile_detail'),
+    path('se/<cec>/edit/', views.edit_profile, name='edit_profile'),
 ]
